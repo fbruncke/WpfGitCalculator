d013d30c1083ccf398941c5fa57d07f07848145a
fbruncke <fha@easv.dk>
2021-10-28 16:14:20 +0200

    works with 2 numbers

--- WpfGitCalculator/MainWindow.xaml.cs
+++ WpfGitCalculator/MainWindow.xaml.cs
@@ -20,9 +20,75 @@ namespace WpfGitCalculator
     /// </summary>
     public partial class MainWindow : Window
     {
+
+        public List<String> CalculatorInput { get; set; } = new List<string>();
+
+
         public MainWindow()
         {
             InitializeComponent();
         }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            Button btn = sender as Button;
+            if (btn != null)
+            {
+                lInput.Content += (btn.Content as String);
+            }
+        }
+
+      
+        
+
+
+        private void Button_Click_Result(object sender, RoutedEventArgs e)
+        {
+            List<Char> charInput = lInput.Content.ToString().ToList<Char>();
+            int no = 0;
+            int no1 = 0;
+            int no2 = 0;
+            int result = 0;
+            Char lastOperator = '!';
+
+            foreach (var item in charInput)
+            {
+                if (item.Equals('+'))
+                {
+                    lastOperator = '+';
+                    no2 = no1;
+                    no1 = 0;
+                }
+                else if (item.Equals('-'))
+                {
+                    lastOperator = '-';
+                    no2 = no1;
+                    no1 = 0;
+                }
+                else if (int.TryParse(item.ToString(), out no))
+                {
+                    no1 = no1 * 10 + no;
+                }
+            }
+
+            if (lastOperator.Equals('+'))
+            {
+                result = no2 + no1;
+            }
+            else if (lastOperator.Equals('-'))
+            {
+                result = no2 - no1;
+            }
+
+
+            this.lResult.Content = result;
+        }
+        private void Button_Clear(object sender, RoutedEventArgs e)
+        {
+            this.lInput.Content = "";
+            
+        }
+
+        
     }
 }

18943064b7458ce69d0476738265bd92c39279d2
fbruncke <fha@easv.dk>
2021-11-19 19:27:09 +0100

    .net mstest

---
+++ UnitMSTest/UnitTest1.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using WpfGitCalculator;
+
+namespace UnitMSTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]        
+        public void PressCalculateWithoutInput()
+        {
+            MainWindowViewModel mainWindowViewModel = new MainWindowViewModel();
+            mainWindowViewModel.CalcResult.Execute(null);
+
+            Assert.AreEqual(mainWindowViewModel.Result, "0");
+        }
+
+        [TestMethod]
+        public void PressCalculateWithInput()
+        {
+            MainWindowViewModel mainWindowViewModel = new MainWindowViewModel();
+            mainWindowViewModel.UserInput = "11+11";
+            mainWindowViewModel.CalcResult.Execute(null);
+
+            Assert.AreEqual(mainWindowViewModel.Result, "22");
+        }
+    }
+}
